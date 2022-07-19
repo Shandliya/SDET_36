b0d2f6310830c3d702f2e723fbf976f5aee9e23c
rites <rites@DESKTOP-4T15L6D>
2022-07-19 11:43:53 +0530

    2nd commit

---
+++ MAVEN_PROJECT/src/test/java/org/mavenExample/TestNgExample.java
@@ -0,0 +1,19 @@
+package org.mavenExample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestNgExample {
+	@Test
+	public void practice1Test()
+	{
+	Reporter.log("Tyss--> test1",true);	
+	}
+	@Test
+	public void practice2Test()
+	{
+		Reporter.log("Tyss-->test2",true);
+	}
+	
+
+}
---
+++ MAVEN_PROJECT/src/test/java/org/mavenExample/TestNgExample2.java
@@ -0,0 +1,26 @@
+package org.mavenExample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestNgExample2 {
+	
+	@Test
+	public void practice3Test()
+	{
+	Reporter.log("Tyss--> test3",true);	
+	}
+	@Test
+	
+	public void practice4Test()
+	{
+		Reporter.log("Tyss-->test4",true);
+		String url = System.getProperty("URL");
+		System.out.println(url);
+		String pwd = System.getProperty("PASSWORD");
+		System.out.println(pwd);
+		
+	}
+	
+
+}
