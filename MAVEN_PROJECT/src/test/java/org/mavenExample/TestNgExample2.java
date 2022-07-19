fbea9acf3e3e7730e8601b0353b3879a98338731
rites <rites@DESKTOP-4T15L6D>
2022-07-19 15:23:57 +0530

    5th commit

--- MAVEN_PROJECT/src/test/java/org/mavenExample/TestNgExample2.java
+++ MAVEN_PROJECT/src/test/java/org/mavenExample/TestNgExample2.java
@@ -23,6 +23,7 @@ public class TestNgExample2 {
 		String hii = System.getProperty("hii");
 		System.out.println("good evening");
 		System.out.println("good morning");
+		System.out.println("good night");
 		
 	}
 	
